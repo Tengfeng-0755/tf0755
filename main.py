8a5276bee032b6c5bcd5b8f2e4ea638a2c1f98a9
Tengfeng-0755 <17171680@qq.com>
2021-01-26 08:37:35 +0800

    web脚本应用的验证，现在调试通过了鼠标移动和点击；键盘输入；

--- main.py
+++ main.py
@@ -64,17 +64,129 @@ def mouse_listener():
 #  鼠标控制函数
 def mouse_press_test():
     ctr = pynput.mouse.Controller()
+    count = 0
+    while   count < 10:
+        # 到达指定位置，按下左键
+        print('墨侠')
+        # move是移动到当前的相对位置，所以要先设定当前位置，再move，move的参数是相对误差（5，-5）
+        print('The current pointer position is {0}'.format(
+            ctr.position))
+        # Set pointer position
+        ctr.position = (1268, 678)  # 移动到墨侠按钮
+        print('Now we have moved it to {0}'.format(
+            ctr.position))
+        # Move pointer relative to current position
+        ctr.move(5, -5)
+        ctr.press(pynput.mouse.Button.left)
+        ctr.release(pynput.mouse.Button.left)   #释放左键。
+        time.sleep(5)
 
-#按下左键。
+        print('寻墨')
+        print('The current pointer position is {0}'.format(
+            ctr.position))
+        # Set pointer position
+        ctr.position = (793, 384)# 移动到寻墨按钮
+        print('Now we have moved it to {0}'.format(
+            ctr.position))
+        # Move pointer relative to current position
+        ctr.move(5, -5)
+        ctr.press(pynput.mouse.Button.left)
+        ctr.release(pynput.mouse.Button.left)   #释放左键。
+        time.sleep(5)
 
-    ctr.move(50, 0)
-#右移50单位。
 
-    ctr.move(0, 50)
-#下移50单位。
-    ctr.press(pynput.mouse.Button.left)
-    ctr.release(pynput.mouse.Button.left)
-#释放左键。
+        print('开始共鸣')
+        print('The current pointer position is {0}'.format(
+            ctr.position))
+        # Set pointer position
+        ctr.position = (1241, 365)  # 移动到开始共鸣按钮
+        print('Now we have moved it to {0}'.format(
+            ctr.position))
+        # Move pointer relative to current position
+        ctr.move(5, -5)
+        ctr.press(pynput.mouse.Button.left)
+        ctr.release(pynput.mouse.Button.left)   #释放左键。
+        time.sleep(5)
+
+        print('开始寻墨')
+        print('The current pointer position is {0}'.format(
+            ctr.position))
+        # Set pointer position
+        ctr.position = (1086, 585)  # 移动到开始寻墨按钮
+        print('Now we have moved it to {0}'.format(
+            ctr.position))
+        # Move pointer relative to current position
+        ctr.move(5, -5)
+        ctr.press(pynput.mouse.Button.left)
+        ctr.release(pynput.mouse.Button.left)   #释放左键。
+        time.sleep(5)
+
+        print('开始寻墨确认')
+        print('The current pointer position is {0}'.format(
+            ctr.position))
+        # Set pointer position
+        ctr.position = (964, 377)  # 移动到开始寻墨确认按钮
+        print('Now we have moved it to {0}'.format(
+            ctr.position))
+        # Move pointer relative to current position
+        ctr.move(5, -5)
+        ctr.press(pynput.mouse.Button.left)
+        ctr.release(pynput.mouse.Button.left)   #释放左键。
+        time.sleep(5)
+
+        print('自动探索')
+        print('The current pointer position is {0}'.format(
+            ctr.position))
+        # Set pointer position
+        ctr.position = (1036, 572)  # 移动到自动探索按钮
+        print('Now we have moved it to {0}'.format(
+            ctr.position))
+        # Move pointer relative to current position
+        ctr.move(5, -5)
+        ctr.press(pynput.mouse.Button.left)
+        ctr.release(pynput.mouse.Button.left)   #释放左键。
+
+        time.sleep(5)
+
+        print('自动探索确认')
+        print('The current pointer position is {0}'.format(
+            ctr.position))
+        # Set pointer position
+        ctr.position = (928, 346)  # 移动到开始寻墨确认按钮
+        print('Now we have moved it to {0}'.format(
+            ctr.position))
+        # Move pointer relative to current position
+        ctr.move(5, -5)
+        ctr.press(pynput.mouse.Button.left)
+        ctr.release(pynput.mouse.Button.left)   #释放左键。
+        time.sleep(240)  #等待寻墨完成
+
+        print('离开')
+        print('The current pointer position is {0}'.format(
+            ctr.position))
+        # Set pointer position
+        ctr.position = (1064, 557)  # 移动到离开按钮
+        print('Now we have moved it to {0}'.format(
+            ctr.position))
+        # Move pointer relative to current position
+        ctr.move(5, -5)
+        ctr.press(pynput.mouse.Button.left)
+        ctr.release(pynput.mouse.Button.left)   #释放左键。
+        time.sleep(2)  #等待寻墨完成
+
+def mouse_point_test():
+    ctr = pynput.mouse.Controller()
+    # Read pointer position
+    print('The current pointer position is {0}'.format(
+        ctr.position))
+
+    # Set pointer position
+    ctr.position = (1268, 678)
+    print('Now we have moved it to {0}'.format(
+        ctr.position))
+
+    # Move pointer relative to current position
+    ctr.move(5, -5)
 
 #模拟滚轮上下滑动
 def mouse_scroll_test():
@@ -125,8 +237,14 @@ def keyboard_press_test():
     #     pass
     # with ctr.pressed('s'):
     #     pass
-    ctr.press('s')
-    ctr.release('s')
+#模拟按键 A
+    # ctr.press('A')
+    # ctr.release('A')
+#模拟特殊按键ctrl+a，效果是当前页面全选
+    with ctr.pressed(pynput.keyboard.Key.ctrl):
+        ctr.press('a')
+        ctr.release('a')
+
     time.sleep(0.3)
 
     with ctr.pressed(pynput.keyboard.Key.esc):
@@ -144,12 +262,13 @@ if __name__ == '__main__':
 #    pynput.mouse.Listener()
     x = "蜀山传奇自动脚本";y = "V1.00"
     print_program_inf(x,y)
-    time.sleep(3)
+    time.sleep(5)
+    # mouse_point_test()
     # mouse_listener()
-    # mouse_press_test()
+    mouse_press_test()
     # mouse_scroll_test()
-    #keyboard_listener()
-    keyboard_press_test()
+    # keyboard_listener()
+    # keyboard_press_test()
 
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
